a95b4b9be2e69625aeb54cd9428f6fe153bb6282
Tony <tonynielsen@u.boisestate.edu>
2022-11-28 10:03:15 -0700

    created index.php

---
+++ index.php
@@ -0,0 +1,13 @@
+<html>
+    <head>
+        <title>Finches Landing Page</title>
+    </head>
+    <body>
+
+    <?php
+
+        echo "Hello! Welcome to the Finches Landing Page, here you can find links to all the finches code examples"
+    ?>
+    
+    </body>
+</html>
