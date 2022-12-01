2c912a11a65f77d2c1f3daa1dcb31db8f3a9db05
kckirch <kckircher@hotmail.com>
2022-11-30 23:50:30 -0700

    index

--- index.php
+++ index.php
@@ -28,7 +28,7 @@
         <br>
         Since PHP can run on many different platforms and with almost all servers, it is very easy to use and operate.
         <br>
-        PHP is an imperative, its functions only have access to their own variables, and is loosely and dynamically typed. Similar to python.
+        PHP is an imperative, its functions only have access to their own variables, and is loosely and dynamically typed. Similar to python. You do not need to tell which data type its variables are.
         <br>
         PHP is interpreted but compiled down to bytecode that is then interpreted by the runtime Zend Engine.
 
@@ -37,7 +37,86 @@
         <b>Installation</b>
         <br>
         Host remotely online or install your own server. Common servers to install are XAMPP, LAMP, WAMP. These often include everything you need, apache, DBs, etc
-        
+        <br>
+        There are also numerous online compiler/ editor sites online that let you test PHP code directly.
+        <br>
+        <br><br>
+        <b>Syntax</b>
+        <br>
+        PHP scripts start with <b>&lt;?php</b> and ends with <b>?></b>
+        <br>
+        The statements end with a semicolon.
+        <br>
+        Certain keywords are not case sensitive like <b>if, while, else, etc.</b>
+        <br>
+        Variable names are case sensitive.
+        <br>
+        <br>
+        <b>Comments</b>
+        <br>
+        Uses common syntax with # , // and /* */. Similar to python, C, and java comments.
+        <br><br>
+        <b>Declaring Variables</b>
+        <br>
+        Variables start with a $, text values need to be assigned with quotes. Once you assign a variable value, it is declared. Like Python
+        <br><br>
+        <b>Output</b>
+        <br>
+        Echo, a built in statement prints data out. Print also prints out data. They are basically the same, Echo returns 0, print returns 1;
+        <br><br>
+        <b>Data Types</b>
+        <br>
+        String, Integer, Float, Boolean, Array, Object, NULL, Resource.
+        <br>
+        Strings can be contained with either double or single quotes.
+        <br><br>
+        <b>Strings</b>
+        <br>
+        PHP's built in string functions are very helpful to use and are similar to C in naming. <br>
+        <br>
+        <b>Math</b>
+        <br>
+        The math functions are plenty and similar to Java formatting for names.
+        <br>
+        Operators are very similar to C and Java. An exponent operator exists as **.
+        <br>
+        Its comparison operators are also similar.
+        <br><br>
+        <b>Constants</b>
+        <br>
+        Constants are declared by using the define(name, value, case-insensitive) function
+        <br><br><br>
+        Statements such as if, if...else, if...elseif...else, and switch all exist. Same with while, do...while, for, foreach
+        <br>
+        <br>
+        <b>Functions</b>
+        <br>
+        Php has over 1000 built in functions that are included without needing to call in anything. 
+        <br>
+        Functions can be created similar format to C. It is created using function functionName(){ };<br>
+        These function names are also NOT case sensitive. Information passed into these functions through arguments.
+        <br><br>
+        <b>Arrays</b><br>
+        array(); is used to create an array. There are many functions that can be used alongside arrays.<br>
+        <br>
+        <b>SuperGlobals</b><br>
+        Set predefined variables in PHP are $GLOBALS
+        $_SERVER 
+        $_REQUEST 
+        $_POST 
+        $_GET 
+        $_FILES 
+        $_ENV 
+        $_COOKIE 
+        $_SESSION <br><br>
+        These are always accessible from any file, class or function.
+
+
+
+
+
+
+
     </body>
 
 
